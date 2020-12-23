b2fa77462de9dd610fa8fc599e46394b1a6ebcd7
Yevheniia123 <zenjunja91@gmail.com>
2020-12-23 20:00:23 +0100

    add correct color switch

--- src/main.js
+++ src/main.js
@@ -1,26 +1,30 @@
 import { colors } from './colors.js';
 
 const refs = {
-    startBtn: document.querySelector('button[data-action="start"'),
-    stopBtn: document.querySelector('button[data-action="stop"'),
-  },
-  randomIntegerFromInterval = max => {
-    return Math.floor(Math.random() * (max + 1));
-  },
-  setRandomColor = () => {
-    const _color = colors[randomIntegerFromInterval(colors.length - 1)];
-    console.log(_color);
-    document.body.style.backgroundColor = _color;
-  };
+  startBtn: document.querySelector('button[data-action="start"'),
+  stopBtn: document.querySelector('button[data-action="stop"'),
+};
+
+function randomIntegerFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function setRandomColor() {
+  const _color = colors[randomIntegerFromInterval(0, colors.length - 1)];
+  console.log(_color);
+  document.body.style.backgroundColor = _color;
+}
 
 let interval = undefined;
 
-refs.startBtn.addEventListener(
-  'click',
-  e =>
-    (interval = interval
-      ? interval
-      : setInterval(() => setRandomColor(), 1000)),
-);
+refs.startBtn.addEventListener('click', () => {
+  interval = setInterval(() => {
+    setRandomColor();
+    refs.startBtn.setAttribute('disabled', true);
+  }, 1000);
+});
 
-refs.stopBtn.addEventListener('click', e => clearInterval(interval));
+refs.stopBtn.addEventListener('click', () => {
+  clearInterval(interval);
+  refs.startBtn.removeAttribute('disabled', true);
+});
